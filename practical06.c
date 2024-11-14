5d50d0d7c7fc9c7b6c0814602aefdb62c8a4a5d0
nilosarma <nilosarma8@gmail.com>
2024-11-14 21:46:42 +0530

    added practical6.c

---
+++ practical06.c
@@ -0,0 +1,19 @@
+/*Write a program to reverse a number*/
+#include<stdio.h>
+int main()
+{
+    int n, r;
+    printf("Enter number");
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+        r=n%10;
+        printf("%d",r);
+        n=n/10;
+        
+    }
+    return 0;
+
+
+}
